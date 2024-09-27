4adeae1845e7bede41d2491ed8b5d98df5255b4b
unknown <g00419296@atu.ie>
2024-09-27 14:23:17 +0000

    Question 3d

--- Lab1/3.js
+++ Lab1/3.js
@@ -3,36 +3,44 @@
 
 const arrayOfStrings = ["john","joe","vincent","mike"];
 
-//created arrow function to add to array
-let addToArray = (newString) =>{
-    
-    arrayOfStrings.push(newString);
-
-}
-
 //method to remove from array based off the index
 let removeFromArray = (indexOfString) =>{
     
     arrayOfStrings.splice(indexOfString);
 }
 
-let displayArray = () =>{
-    
-        arrayOfStrings.forEach((item)=>{
-        console.log(item);
-    })
-}
-
-let addTask = () =>{
-    addToArray("Task");
+//created arrow function to add to array
+let addTask = (string) =>{
+    arrayOfStrings.push(string);
     console.log("Task has been logged into the array \n Number of elements = " + arrayOfStrings.length +"\n");
 
 }
 
+//displays all the tasks
 let listAllTasks = () => {
 
-    displayArray();
+    arrayOfStrings.forEach((item)=>{
+        console.log(item);
+    })
 }
 
-addTask();
+//method to delete tasks 
+let deleteTask = (string) =>{
+    var counter =-1 ;
+
+    arrayOfStrings.forEach((item)=>{
+        counter + 1;
+
+        if(item == string){
+            console.log(counter);
+            arrayOfStrings.splice(counter,1);
+        }
+    })
+    console.log("Task has been removed from the array \n Number of elements = " + arrayOfStrings.length +"\n");
+}
+
+addTask("Tasks");
+listAllTasks();
+
+deleteTask("Tasks");
 listAllTasks();
