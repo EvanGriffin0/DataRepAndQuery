373657d64fb18ddb3060a8ffbf063efb3561f905
unknown <g00419296@atu.ie>
2024-09-27 13:32:55 +0000

    Question 2b

---
+++ Lab1/2b.js
@@ -0,0 +1,9 @@
+
+//created a arrow function to print the statement
+let arrowFunction = (value) => {
+    console.log(value);
+}
+
+var myString = "Data Representation and Querying"
+
+arrowFunction(myString);
