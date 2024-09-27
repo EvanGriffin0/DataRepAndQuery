e5773bd983409dabfdb2c07f1cd15a351a4d46a3
unknown <g00419296@atu.ie>
2024-09-27 14:12:51 +0000

    Question 3b

---
+++ Lab1/3.js
@@ -0,0 +1,34 @@
+
+//create functions to add to it ,remove from it and then display the array
+
+const arrayOfStrings = ["john","joe","vincent","mike"];
+
+//created arrow function to add to array
+let addToArray = (newString) =>{
+    
+    arrayOfStrings.push(newString);
+
+}
+
+//method to remove from array based off the index
+let removeFromArray = (indexOfString) =>{
+    
+    arrayOfStrings.splice(indexOfString);
+}
+
+let displayArray = () =>{
+    
+        arrayOfStrings.forEach((item)=>{
+        console.log(item);
+    })
+}
+
+let addTask = () =>{
+
+    addToArray("Task");
+    displayArray();
+    console.log("Task has been logged into the array \n Number of elements = " + arrayOfStrings.length);
+
+}
+
+addTask();
